92ecce8ffc6a5ae1b3f5eec64fcb4f961fb74b37
firecomputer <jay061116@gmail.com>
2025-03-12 14:03:08 +0900

    added few files

---
+++ basic_calculating.py
@@ -0,0 +1,10 @@
+# 입력
+input_a = float(input("첫 번째 숫자> "))
+input_b = float(input("두 번째 숫자> "))
+
+# 결과 출력
+print("### 출력 결과")
+print(f"덧셈 결과: {input_a + input_b}")
+print(f"뺄셈 결과: {input_a - input_b}")
+print(f"곱셈 결과: {input_a * input_b}")
+print(f"나눗셈 결과: {input_a / input_b}")
