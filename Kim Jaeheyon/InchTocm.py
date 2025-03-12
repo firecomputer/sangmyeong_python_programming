c291b2357180e6e262c5137927732be03d814576
firecomputer <jay061116@gmail.com>
2025-03-12 17:17:16 +0900

    finally totally finished my homework

---
+++ Kim Jaeheyon/InchTocm.py
@@ -0,0 +1,20 @@
+# Getting inch from input
+print("# 인치를 센치미터로 변환하는 프로그램입니다.")
+inch = float(input("먼저 인치(inch)를 입력해주세요: "))
+
+# Calculating
+print()
+print()
+print()
+print("처리 중입니다...")
+cm = inch * 2.54 # inch multiple 2.54 equals to cm
+
+# Printing Result
+print()
+print()
+print()
+print("결과는 다음과 같습니다:")
+print(f"변환 결과: {cm}cm")
+
+# Stopping Program
+print("프로그램을 종료합니다.")
---
+++ Kim Jaeheyon/kgToPound.py
@@ -0,0 +1,20 @@
+# Getting kg from input
+print("# 킬로그램을 파운드로 변환하는 프로그램입니다.")
+kg = float(input("먼저 킬로그램(kg)을 입력해주세요: "))
+
+# Calculating
+print()
+print()
+print()
+print("처리 중입니다...")
+pound = kg * 2.205 # kg multiple 2.205 similar to pound
+
+# Printing Result
+print()
+print()
+print()
+print("결과는 다음과 같습니다:")
+print(f"변환 결과: {pound}lbs")
+
+# Stopping Program
+print("프로그램을 종료합니다.")
