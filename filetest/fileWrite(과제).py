1985fbe9cc5c4614f7a1d67d5dbaace361f8c725
jaeyoonpark2006 <parkjaeyoon2006@gmail.com>
2025-05-20 17:42:33 +0900

    z

--- filetest/fileWrite(과제).py
+++ filetest/fileWrite(과제).py
@@ -1,7 +1,9 @@
 outFp = None
 outStr = ""
 
-outFp = open('C:\\Users\\박재혁\\Desktop\\sangmyeong_python_programming\\filetest\\data2.txt', 'w', encoding="cp949")
+fname = input('파일명을 입력하세요')
+
+outFp = open('C:\\Users\\박재혁\\Desktop\\sangmyeong_python_programming\\filetest\\' + fname, 'w', encoding="UTF8")
 
 while True:
     outStr = input('내용 입력:')
